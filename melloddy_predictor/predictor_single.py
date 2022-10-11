ab0cbaad6bb651e47249d79c085647f6b459dbf3
ansgar.schuffenhauer <ansgar.schuffenhauer@novartis.com>
2022-10-11 17:00:45 +0200

    added Enum type class for model/task type encoding

--- melloddy_predictor/predictor_single.py
+++ melloddy_predictor/predictor_single.py
@@ -9,12 +9,14 @@ import argparse
 from scipy.sparse import csr_matrix
 from scipy.special import expit
 from collections import OrderedDict
+from enum import Enum
 import warnings
+
 
 def csr_to_torch_coo(csr_mat: csr_matrix) -> torch.Tensor:
     """
     Converts from scipy sparse csr matrix to a torch sparsae_coo_tensor to be submitted to the sparsechem network
-	Sparsewchem requires 
+    Sparsewchem requires 
     
     Args:
         csr_matx (scipy.sparse.csr_matrix) sprse csr matrix to convert
@@ -26,6 +28,12 @@ def csr_to_torch_coo(csr_mat: csr_matrix) -> torch.Tensor:
     return torch.sparse_coo_tensor(indices = np.array([coo_mat.row,coo_mat.col]), values = coo_mat.data, size = coo_mat.shape, dtype=torch.float)
 
 
+class ScModelType(Enum):
+    classification = 0
+    regression = 1
+    hybrid = 2
+
+  
 class PredictorSingle:
     """
     This class handles predictions for single instances.
@@ -77,7 +85,14 @@ class PredictorSingle:
         
         if self.net.cat_id_size is not None:
             raise NotImplementedError("Predictions for models with a catalog head are not yet implemented")
-        
+        if self.net.class_output_size > 0:
+            if self.net.regr_output_size > 0:
+                self.model_type = ScModelType.hybrid
+            else:
+                self.model_type = ScModelType.classification
+        elif self.net.regr_output_size > 0:
+            self.model_type = ScModelType.regression
+
         self.has_task_maps = False
         if (class_task_map is not None) or (class_task_map is not None):
             self.set_tasks_maps(class_task_map, regr_task_map)
@@ -92,43 +107,89 @@ class PredictorSingle:
             regr_task_map:               a dictionary or pandas series having regression task labels as as key or index, resp, and continuous regression task IDs (column indexes of the prediction matrix) as values 
         
         """
-        class_task_map, regr_task_map = self.__validate_maps__(class_task_map, regr_task_map)
+        class_task_map, regr_task_map, mapped_tasks_type = self.__validate_maps__(class_task_map, regr_task_map)
         self.class_task_map = class_task_map
         self.regr_task_map = regr_task_map
+        self.mapped_tasks_type = mapped_tasks_type
         self.has_task_maps = True
 
-        
+
+    def get_mapped_task_names(self):
+        if not self.has_task_maps:
+            return None
+        elif self.mapped_tasks_type == ScModelType.classification:
+            return class_task_map.index.values
+        elif self.mapped_tasks_type == ScModelType.regression:
+            return regr_task_map.index.values
+        elif self.mapped_tasks_type == ScModelType.hybrid:
+            return self.mapped_task_type_info.index.values
+
+    def get_model_type(self):
+        return self.model.type
+
+    def get_mapped_task_type(self):
+        if self.has_task_maps:
+            return self.mapped_tasks_type
+        else:
+            return None
+
+    def get_num_class_tasks(self):
+        return self.conf.class_output_size
+
+    def get_num_regr_tasks(self):
+        return self.conf.regr_output_size
+
+    def get_num_tasks(self):
+        return self.conf.class_output_size + self.conf.regr_output_size
+
+    def get_num_tasks_by_type(self, type):
+        if type == ScModelType.classification:
+            return self.get_num_class_tasks()
+        elif type == ScModelType.regression:
+            return self.get_num_regr_tasks()
+        else:
+            raise ValueError("Non permitted task type {}".format(type))
+
     def __validate_maps__(self,class_task_map, regr_task_map):
-        class_task_map = self.__validate_map__(class_task_map, "classification", self.conf.class_output_size)
-        regr_task_map = self.__validate_map__(regr_task_map, "regression", self.conf.regr_output_size)
+        class_task_map = self.__validate_map__(class_task_map, ScModelType.classification)
+        regr_task_map = self.__validate_map__(regr_task_map, ScModelType.regression)
         #test for non overlap of task labels between classification and regression
-        if (class_task_map is not None) and (regr_task_map is not None):
+        if class_task_map is not None:
+            if regr_task_map is not None:
+                mapped_tasks_type = ScModelType.hybrid
+            else:
+                mapped_tasks_type = ScModelType.classification
+        else:
+            if regr_task_map is not None:
+                mapped_tasks_type = ScModelType.regression
+            else:
+                raise ValueError("Task maps for both classification and regression are None")
+        if mapped_tasks_type == ScModelType.hybrid:
             if class_task_map.index.intersection(regr_task_map.index).size > 0:
-                raise ValueError("classification and regression task map have task labels in common")
-        return class_task_map, regr_task_map
+                raise ValueError("classification and regression task map have task labels in common, this is not permitted")
+        return class_task_map, regr_task_map, mapped_tasks_type
     
-    
-    @staticmethod    
-    def __validate_map__(task_map, category, category_output_size):
+
+    def __validate_map__(self, task_map, task_type):
         if task_map is not None:
-            if category_output_size == 0:
-                raise ValueError("A {0} task map has been provided for a model without {0} tasks".format(category))
+            if self.get_num_tasks_by_type(task_type) == 0:
+                raise ValueError("A {0} task map has been provided for a model without {0} tasks".format(task_type.name))
             if type(task_map) == pd.Series:
                 pass
             elif type(task_map) == dict:
                 task_map = pd.Series(task_map)
             else:
                 raise TypeError("{0} task_map needs be either of type pandas.Series or be a dictionary, but is a {1}".\
-                                 format(category, type(task_map)))
+                                 format(task_type.name, type(task_map)))
             if not task_map.dtype == int:
-                raise TypeError("The {0} task_map needs to have values of type int".format(category))
-            if task_map.max() >= category_output_size:
+                raise TypeError("The {0} task_map needs to have values of type int".format(task_type.name))
+            if task_map.max() >= self.get_num_tasks_by_type(task_type):
                 raise ValueError("The maximum value of {0} task_map exceeps the number of {0} outputs ({1})".\
-                                 format(category,category_output_size))
+                                 format(task_type.name,self.get_num_tasks_by_type(task_type)))
             if not task_map.is_unique:
-                raise ValueError("the task indexes in {0} task map are not unique".format(category))
+                raise ValueError("the task indexes in {0} task map are not unique".format(task_type.name))
             if not task_map.index.is_unique:
-                raise ValueError("the task labels in {0} task map are not unique".format(category))
+                raise ValueError("the task labels in {0} task map are not unique".format(task_type.name))
         return task_map
                                                                  
 
@@ -172,8 +233,11 @@ class PredictorSingle:
                 y_regr_array = y_regr_array * self.reg_stddev + self.reg_mean
         return y_class_array, y_regr_array
 
+    @staticmethod
+    def extract_tasks(y_array, task_map):
+         return pd.Series(y_array[0,task_map.values],index = task_map.index)
     
-    def predict_decorated_series_from_tensor(self, X: torch.Tensor, class_task_map=None, regr_task_map=None) -> pd.Series:
+    def predict_decorated_series_from_tensor(self, X: torch.Tensor, class_task_map=None, regr_task_map=None, limit_to_type = None) -> pd.Series:
         """
         This runs the prediction on the input tensor expected to have single row and extracts the desired tasks based on the information in the task 
         maps that have been passed either on predictor intitialization, or with the call of this function (having precedence). 
@@ -190,34 +254,39 @@ class PredictorSingle:
         """
         #This function expects receiving a tensor witrh a single row, as we also only return results for the first row 
         if X.size(0) != 1:
-            raise ValueError("This function expects only single row tensor, but te tensor passed has size of {0}".format(X.size(0)))
+            raise ValueError("This function expects only single row tensor, but the tensor passed has size of {0}".format(X.size(0)))
 
         #if task maps are passed as arguments we use them
         if (class_task_map is not None) or (regr_task_map is not None):
-            class_task_map, regr_task_map = self.__validate_maps__(class_task_map, regr_task_map)
+            class_task_map, regr_task_map, mapped_tasks_type = self.__validate_maps__(class_task_map, regr_task_map)
         #otherwise we fall back to the tasks maps passed upon intialization, if peresent
         elif self.has_task_maps:
             class_task_map = self.class_task_map
             regr_task_map = self.regr_task_map
+            mapped_tasks_type = self.mapped_tasks_type
         #if those don't exist, the function cannpot proceed
         else:
             raise ValueError("Task maps must be passed either at intialization time of the predictor or when calling the prediction functions")
                
         y_class_array, y_regr_array = self.predict_from_tensor(X)
         
-        if class_task_map is not None:
-            y_class_series = pd.Series(y_class_array[0,class_task_map.values],index = class_task_map.index)
-        else:
-            y_class_series = None
-        if regr_task_map is not None:
-            y_regr_series = pd.Series(y_regr_array[0,regr_task_map.values],index = regr_task_map.index)
-        else:
-            y_regr_series = None
-        #pd.concat drops silently None items, unless all items to concat are None
-        return pd.concat([y_class_series, y_regr_series])
+        if mapped_tasks_type == ScModelType.hybrid and limit_to_type is not None:
+            if mapped_type in [ScModelType.classification,ScModelType.regression]:
+                mapped_tasks_type = limit_to_type
+            else:
+                raise ValueError("Not permitted type {0} has been provided for limit_to_type".format(limit_to_type))
+
+        if mapped_tasks_type == ScModelType.classification:
+             results = self.extract_tasks(y_class_array, class_task_map)
+        elif mapped_tasks_type == ScModelType.regression:
+             results = self.extract_tasks(y_regr_array, regr_task_map)
+        elif mapped_tasks_type == ScModelType.hybrid:
+             results = pd.concat([self.extract_tasks(y_class_array, class_task_map),self.extract_tasks(y_regr_array, regr_task_map)])
+
+        return results
 
     
-    def predict_decorated_series_from_csr(self, x_csr: csr_matrix, class_task_map=None, regr_task_map=None) -> pd.Series:
+    def predict_decorated_series_from_csr(self, x_csr: csr_matrix, class_task_map=None, regr_task_map=None, limit_to_type = None) -> pd.Series:
         """
         This runs the prediction on the input csr_matrix expected to have single row and extracts the desired tasks based on the information in the task 
         maps that have been passed either on predictor intitialization, or with the call of this function (having precedence). 
@@ -233,10 +302,10 @@ class PredictorSingle:
             pd.Series of predictions with task labels as index        
         """
         X = csr_to_torch_coo(x_csr)
-        return self.predict_decorated_series_from_tensor(X, class_task_map, regr_task_map)
+        return self.predict_decorated_series_from_tensor(X, class_task_map, regr_task_map, limit_to_type = limit_to_type)
             
 
-    def predict_hidden_from_tensor(self, X: torch.Tensor) -> np.ndarray:
+    def predict_last_hidden_from_tensor(self, X: torch.Tensor) -> np.ndarray:
         """
         This function computes the last hidden layer of the model
         
@@ -248,10 +317,10 @@ class PredictorSingle:
         """
         
         with torch.no_grad():
-            return self.net(X.to(self.device), last_hidden=True).cpu().numpy()            
+            return self.net(X.to(self.device), last_hidden=True).cpu().numpy()
 
     
-    def predict_hidden_from_csr(self, x_csr):
+    def predict_last_hidden_from_csr(self, x_csr):
         """
         This function computes the last hidden layer of the model
         
@@ -265,6 +334,33 @@ class PredictorSingle:
         return self.predict_hidden_from_tensor(X)        
                 
 
+    def predict_trunk_from_tensor(self, X: torch.Tensor) -> np.ndarray:
+        """
+        This function computes the last hidden layer of the model
+        
+        Args:
+            X (torch.sparse_coo_tensor) : fingerprint features as tporch sparse_coo_tensor
+        
+        Returns:
+            numpy.ndarray of hidden layer values
+        """
+        
+        with torch.no_grad():
+            return self.net(X.to(self.device), trunk_embeddings=True).cpu().numpy()
+
+    
+    def predict_trunk_from_csr(self, x_csr):
+        """
+        This function computes the last hidden layer of the model
+        
+        Args:
+            x_csr (scipy.sparse.csr_matrix) : fingerprint features as csr_matrix
+        
+        Returns:
+            numpy.ndarray of hidden layer values
+        """
+        X = csr_to_torch_coo(x_csr)
+        return self.predict_hidden_from_tensor(X)        
         
         
 
